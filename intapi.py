2164849b6196de14ec7edb3a2148bcf2b0acd344
kkaz2611 <58026787+kkaz2611@users.noreply.github.com>
2019-11-21 15:01:48 +0100

    Add files via upload
    pierwszy commit

---
+++ intapi.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import maya
+import datetime
+import arrow
+
+print("Podaj login")
+user = input()
+link = "https://api.github.com/users/" + user
+response = requests.get(link)
+
+x = response.json()
+
+print(x["login"])
+print(x["name"])
+print(x["avatar_url"])
+dt = maya.parse(x["created_at"]).datetime()
+print(dt.date())
+
+print('')
+dt = maya.parse(x["created_at"]).datetime()
+a = arrow.get(datetime.date.today())
+print(a)
+b = arrow.get(dt.date())
+print(b)
+
+delta = (a-b)
+
+print(delta.days)
+
+# do poprawienia przypadek godzinowy
